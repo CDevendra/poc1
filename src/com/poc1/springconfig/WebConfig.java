a868436cdcc62918db79abeaf7d0ced624553d12
CDevendra <deven.chandode@gmail.com>
2018-10-29 20:56:43 +0530

    remove unwanted sql script

--- src/com/poc1/springconfig/WebConfig.java
+++ src/com/poc1/springconfig/WebConfig.java
@@ -27,7 +27,7 @@ public class WebConfig implements WebMvcConfigurer  {
 	 */
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/poc1/").setViewName("home");
 	}
 	
 	@Bean
